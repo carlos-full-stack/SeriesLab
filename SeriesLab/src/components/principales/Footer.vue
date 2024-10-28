11fa7cff63ebe0dcf9b67f6498646f6c17b28f34
xpilasi <pilasiha@gmail.com>
2024-10-28 13:22:30 +0100

    removing dummydata 1

---
+++ SeriesLab/src/components/principales/Footer.vue
@@ -0,0 +1,22 @@
+<script >
+//Aquí importamos el componente Title
+
+
+export default {
+
+  //Aquí llamamos a todos los componentes
+    components:{
+   
+    data(){
+        return{
+            
+        }
+    },
+}
+</script>
+
+<template>
+  <div >Esto es un Footer</div>
+    
+
+</template>
