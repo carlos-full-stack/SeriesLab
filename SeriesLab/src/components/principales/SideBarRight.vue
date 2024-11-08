657f3fa3197789b4f444a3d437d8b07e17918246
Chally <carlosgilino@gmail.com>
2024-11-08 21:07:30 +0100

    add sidebar changes

--- SeriesLab/src/components/principales/SideBarRight.vue
+++ SeriesLab/src/components/principales/SideBarRight.vue
@@ -1,11 +1,4 @@
 <script >
-<<<<<<< HEAD
-
-=======
-import SearchBar from './widgets/SearchBar.vue';
-import CardUserAllLastReviews from './Cards/CardUserAllLastReviews.vue';
-import CardTopReviews from './Cards/CardTopReviews.vue';
->>>>>>> feature/sidebar
 
 export default {
     //Estos son los parámetros que se le pasan al componente
@@ -19,13 +12,7 @@ export default {
     },
     //Aquí van los componentes importados
     components:{
-<<<<<<< HEAD
     
-=======
-        SearchBar,
-        CardUserAllLastReviews,
-        CardTopReviews
->>>>>>> feature/sidebar
 
     }
 }
