dc14e35cd1d8d524bc92dc0708fae0903bf42d08
Chally <carlosgilino@gmail.com>
2024-11-13 19:41:39 +0100

    create sidebar components

--- SeriesLab/src/components/principales/SideBarRight.vue
+++ SeriesLab/src/components/principales/SideBarRight.vue
@@ -1,5 +1,10 @@
 <script >
 
+import LastReviewsContainer from './listas/LastReviewsContainer.vue';
+import UserLastReviewsContainer from './listas/UserLastReviewsContainer.vue';
+import Login from './widgets/Login.vue';
+import SearchBar from './widgets/SearchBar.vue';
+
 export default {
     //Estos son los parámetros que se le pasan al componente
     props:{
@@ -13,14 +18,21 @@ export default {
     //Aquí van los componentes importados
     components:{
     
+        LastReviewsContainer,
+        UserLastReviewsContainer,
+        SearchBar,
+        Login
 
     }
 }
 </script>
 <template>
     
-    <div class=" bg-secondaryBackground h-full">
-        SIDEBAR
+    <div class=" bg-secondaryBackground h-full flex flex-col gap-y-10 items-center justify-center">
+        <SearchBar />
+        <Login />
+        <UserLastReviewsContainer />
+        <LastReviewsContainer />
     </div>
   
 </template>
