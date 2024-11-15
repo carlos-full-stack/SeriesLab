cb643a8144f257c4d545f43472f6e19037e9c2d7
xpilasi <pilasiha@gmail.com>
2024-11-15 20:02:37 +0100

    main 1.0 con links

--- SeriesLab/src/components/principales/Main.vue
+++ SeriesLab/src/components/principales/Main.vue
@@ -1,22 +1,40 @@
 <script >
 import TopReviewsSeries from './listas/TopReviewsSeries.vue';
 import TopRatedSeries from './listas/TopRatedSeries.vue';
+import { getFirestore, collection, getDocs, addDoc } from 'firebase/firestore';
+import { app } from '../../../firebase/firebase.js';
 
+//API
 //Conexión con la API de TMDB al recurso de las Top series
 const DB_TMDB_TOP_RATED= 'https://api.themoviedb.org/3/tv/top_rated?api_key='
-// const API_KEY = 'f1a529d2a0fbae65199fc5360a807700'
 const API_KEY = 'de4b6461e071a300a503b91fb095113d'
 const LANGUAGE = 'es-ES'; // Idioma español de España
 const DB_TOP_RATED = `${DB_TMDB_TOP_RATED}${API_KEY}&language=${LANGUAGE}`
-//  token xpilasidev: de4b6461e071a300a503b91fb095113d
-//  token acceso de lectura a la API: eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZTRiNjQ2MWUwNzFhMzAwYTUwM2I5MWZiMDk1MTEzZCIsIm5iZiI6MTczMTY1MDA4OS4wMDI0MzQ3LCJzdWIiOiI2NzM2ZTBhN2ZmZTM4NzhlOWU5ZmIxZGUiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.t6Loa4DIT20E-RzR-0sTsG3BmjlQWqOOFJLupCqjO_Y
+
+
+//FIREBASE:
+ //Conexión con la app de Firestore que se encuentra en el firebase.js
+ const db = getFirestore(app);
+ //Entrega un array con todos los objetos de la colección usuario de Firebase OK
+ async function getTodos() {
+    const todosCollectionRef = collection(db, 'all-reviews-series');
+    const todosSnapshot = await getDocs(todosCollectionRef);
+    const todasLasReviewsArray = todosSnapshot.docs.map(doc => ({
+      id: doc.id,
+      ...doc.data()
+    }));
+    return todasLasReviewsArray;
+  }
 
 
 export default {
     data(){
         return{
 
-            arrayTopRated: []
+            arrayTopRated: [],
+            allReviewsFirestore: [],
+            
+
         }
     },
     //Estos son los parámetros que se le pasan al componente
@@ -38,6 +56,8 @@ export default {
       
     },
     created() {
+
+        //Llamada a la API:
             fetch(DB_TOP_RATED)
             .then(res=>res.json())
             .then(data=>{
@@ -45,14 +65,30 @@ export default {
                 this.arrayTopRated = data.results.slice(0,6)
                 
             })
+
+        //Llamada al Firestore:
+            setTimeout(()=>{
+
+                getTodos().then(todosData => {
+                this.allReviewsFirestore = todosData.slice(0,4)
+
+                //Comprobando el array de objetos que son todas las reviews:
+                console.log(this.allReviewsFirestore);
+                
+                });
+                },1000)
+
+
         },
 }
 </script>
 <template>
     
-    <div class="px-20 min-h-[calc(100vh-14rem)] flex flex-col gap-20 justify-center align-middle w-full ">
+    <div class="px-20 min-h-[calc(100vh-14rem)] flex flex-col gap-20 justify-center pb-16 align-middle w-full ">
         <!-- Xavi -->
-        <TopReviewsSeries/>
+        <TopReviewsSeries
+        :arrayFromFirestore="allReviewsFirestore"
+        />
 
         <!-- Mary -->
         <TopRatedSeries 
