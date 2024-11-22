80051d275e544215d68fbb8ee743ff38fc80a33e
xpilasi <pilasiha@gmail.com>
2024-11-22 23:38:08 +0100

    details

--- SeriesLab/src/components/pages/SearchResults.vue
+++ SeriesLab/src/components/pages/SearchResults.vue
@@ -96,7 +96,7 @@ export default {
 }
 </script>
 <template>
-    <div class="lg:px-20 hidden px-5 h-20 md:px-10   flex-row justify-between items-center bg-gradient-to-b from-gray-900 to-transparent ">
+    <div class="lg:px-20 hidden lg:block px-5 h-20 md:px-10 lg:py-5    flex-row justify-between items-center bg-gradient-to-b from-gray-900 to-transparent ">
             <RouterLink to="/">
                 <LogoSeriesLab class="pt-1"/>
             </RouterLink>
@@ -104,11 +104,11 @@ export default {
 
     </div>
     
-    <div v-if="!loading" class="md:px-10 px-5 min-h-[calc(100vh-14rem)] flex flex-col gap-4 justify-start pt-5 align-middle w-full lg:px-20">
+    <div v-if="!loading" class="md:px-10 px-5 min-h-[calc(100vh-14rem)] flex flex-col gap-4 justify-start lg:pt-5 align-middle w-full lg:px-20">
         <div class="flex flex-row">
 
-            <div  class="py-5">Resultados de la búsqueda:</div>
-            <div  class=" py-5 text-primary ml-3 "> {{ query }}</div>
+            <div  class="lg:py-5">Resultados de la búsqueda:</div>
+            <div  class=" lg:py-5 pb-3 text-primary ml-3 "> {{ query }}</div>
         </div>
         <div class="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-6  gap-4 ">
 
